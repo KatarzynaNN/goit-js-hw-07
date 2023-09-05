c5bff36518f238295670da089b36dd33a4eddb07
KatarzynaNN <kat.nedza@gmail.com>
2023-09-05 14:41:59 +0200

    task 2 added simple light box

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
@@ -13,3 +13,9 @@ const markup = galleryItems
 	.join("");
 
 galleryList.innerHTML = markup;
+
+let lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionPosition: "bottom",
+	captionDelay: 250,
+});
