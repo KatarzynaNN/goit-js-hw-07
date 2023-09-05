1ed96f6b97064d17e5368b67f8d8219cc1b6f4f2
KatarzynaNN <kat.nedza@gmail.com>
2023-09-05 11:19:02 +0200

    task nr 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems
+	.map(
+		(item) =>
+			`<li class="gallery__item"><a class="gallery__link" href=${item.original}><img class="gallery__image" src=${item.preview} data-source=${item.original} alt=${item.description}/></a></li>`
+	)
+	.join("");
+
+galleryList.innerHTML = markup;
+
+galleryList.addEventListener("click", (event) => {
+	event.preventDefault();
+	//tylko kliknięcie w img
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+	// z dokumentacji lightbox
+	const content = document.createElement("div");
+	const img = document.createElement("img");
+	img.src = event.target.getAttribute("data-source");
+	content.append(img);
+	const instance = basicLightbox.create(content);
+	instance.show();
+
+	// zamknięcie okna przyciskiem esc
+	document.addEventListener("keydown", (event) => {
+		if (event.key === "Escape") {
+			instance.close();
+		}
+	});
+});
